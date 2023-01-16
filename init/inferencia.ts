5761edb9879a6cf6ba0e95731cf9afc49546aef3
aleff menna resler <83561751+Aleff13@users.noreply.github.com>
2023-01-16 12:57:46 -0300

    Update inferencia.ts

--- init/inferencia.ts
+++ init/inferencia.ts
@@ -1,14 +1,14 @@
-//quando trabalhamos com o ts ele nor permite tipas variaveis como no exemplo abaixo
+//Quando trabalhamos com o ts ele nos permite tipar variaveis como no exemplo abaixo
 let age: number = 20;
 let username: string = "joao";
 let isBrazilian: boolean = true;
 
-//porém o typescript consegue validar já na atribuição da variavel e de seu valor qual será o tipo dela
+//Porém o typescript consegue validar já na atribuição da variavel e de seu valor qual será o tipo dela
 
 let anos = 20;
 let nome = "joao";
 let ehBrasileiro = true;
 
-//caso não coloquemos nenhum valor e nenhum tipo em sua criação ela será declarada com o tipo any
+//Caso não coloquemos nenhum valor e nenhum tipo em sua criação ela será declarada com o tipo any
 
 let value;
