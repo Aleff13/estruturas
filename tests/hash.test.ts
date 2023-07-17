16fca31216673a4e61ecc2d386a27300d2f8d939
Aleff13 <aleff.menna.resler@gmail.com>
2023-07-17 13:25:17 -0300

    test: removes a record

--- tests/hash.test.ts
+++ tests/hash.test.ts
@@ -1,11 +1,23 @@
 import { describe, expect, test } from "@jest/globals";
 import HashTable from "../Hash/HashTable";
 
-test("Should put key, value in the hashTable", () => {
+test("Should put record in the hashTable", () => {
   const hash = new HashTable();
+
   const key = "teste";
   const value = "123";
   hash.put(key, value);
 
   expect(hash.get(key)?.value).toBe(value);
+});
+
+test("Should remove a record from the hashTable", () => {
+  const hash = new HashTable();
+
+  const key = "teste";
+  const value = "123";
+  hash.put(key, value);
+
+  hash.remove(key);
+  expect(hash.get(key)).toBe(undefined);
 });
